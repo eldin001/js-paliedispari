e0dd0fbeb1b39d522418ef56af89b67835d0d8bb
eldin001 <eldin.balje@icloud.com>
2024-03-14 13:57:45 +0100

    user request

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+const parola = prompt('Inserisci una parola per sapere se è pari o dispari:');
+
+
+

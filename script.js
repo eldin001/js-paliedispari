2b46e9fdb908af3c899b6f6be4eb82f299fe54f9
eldin001 <eldin.balje@icloud.com>
2024-03-14 14:00:42 +0100

    added function to revert and goin letters

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
 const parola = prompt('Inserisci una parola per sapere se è pari o dispari:');
 
+function palindroma(parola) {
+    return parola === parola.split("").reverse().join();
+}
 
 

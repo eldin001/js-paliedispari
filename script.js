669d3b9013faf076c931eb8e19f636f53060e1d8
eldin001 <eldin.balje@icloud.com>
2024-03-14 14:13:03 +0100

    user request

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 /* Palidroma
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma */
-const parola = prompt('Inserisci una parola per sapere se è pari o dispari:');
+/* const parola = prompt('Inserisci una parola per sapere se è pari o dispari:');
 
 function palindroma(parola) {
     return parola === parola.split("").reverse().join();
 }
+
+if (palindroma(parola)) {
+    console.log("La parola è palindroma!");
+} else {
+    console.log("La parola non è palindroma.");
+} */
+
+/* Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto. */
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+let scelta = prompt('Scegli se pari o dispari');
+
+let numero = parseInt(prompt('Scegli un numero da 1 a 5'));
+
+let numeroDelComputer = getRndInteger(1, 5);
+
+
 
 
